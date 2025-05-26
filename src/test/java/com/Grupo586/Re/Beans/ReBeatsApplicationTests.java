2b93e871aa831e3500f80e1e3ceb19bf164b84f0
Ricardo <ricaardoandresma@gmail.com>
2025-05-26 19:38:17 -0400

    Json admin, agregado, cambio de name en rebeats

--- src/test/java/com/Grupo586/Re/Beans/ReBeatsApplicationTests.java
+++ src/test/java/com/Grupo586/Re/Beans/ReBeatsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReBeansApplicationTests {
+class ReBeatsApplicationTests {
 
 	@Test
 	void contextLoads() {
